f96da0dfdbf3e260b74810ebe2ff193e4955de02
Ali <muhammadali.pc64@gmail.com>
2024-04-13 11:24:20 +0500

    greet html P

--- movie_recommendations/home/views.py
+++ movie_recommendations/home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, "home/index.html")
+def greet(request, name):
+    return render(request, "home/greet.html" ,{
+        "name" : name.capitalize()
+    })
