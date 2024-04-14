92733075606363d53104013b74b1769c5779d08e
Ali <muhammadali.pc64@gmail.com>
2024-04-14 13:34:06 +0500

    urls linked watch later p

--- movie_recommendations/watch_later/views.py
+++ movie_recommendations/watch_later/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+
+watch_later=[1,2]
 # Create your views here.
 def list(request):
-    return render(request, "watch_later/index.html")
+    return render(request, "watch_later/index.html",{
+        "list":watch_later
+    })
